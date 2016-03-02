cbb735d694109548d37d117cbd5ac536e13411d0
Remi OSWALT <remi.oswalt@etudiant.univ-lille1.fr>
2016-03-02 16:17:39 +0100

    Debut de GET

--- webserveur/main.c
+++ webserveur/main.c
@@ -13,29 +13,49 @@ int main(void){
   int socket_serveur=creer_serveur(8080);	
   int socket_client ;
   char buffer[1024] ="";
+  const char *msg400 = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad Request\r\n";
+  const char *msg200 = "HTTP/1.1 200 OK\r\nContent-Length: length";
+  char *methode;
+  char *ressources;
+  char *spl;
 
-  while(1){
-    socket_client= accepte_client(socket_serveur);
+  while( (socket_client= accepte_client(socket_serveur))!= -1 ){
     if(fork()==0){
       FILE * fd=fdopen(socket_client,"w+");
+      fgets(buffer, sizeof(buffer),fd);
+
+      methode = strtok(buffer, " ");
+      strtok(NULL, " ");
+      ressources = strtok(NULL, " ");
+      
+      spl = strtok(NULL, " ");
+      
+      if ((strcmp(methode,"GET")==0) && (ressources != NULL) && (spl == NULL)){
+	if ((strcmp(ressources,"HTTP/1.0")==0) || (strcmp(ressources,"HTTP/1.1")==0)){
+	  fprintf(fd, "%s\n", msg200 );
+	  fflush(fd);
+	  fclose(fd);
+	}
+      } else {
+	fprintf(fd,"%s\n",msg400);
+	fflush(fd);
+	fclose(fd);
+      }
+      /*
       while(strcmp("fin\n",buffer)!=0){	
 	if( fgets(buffer, sizeof(buffer), fd) == NULL){
 	  return -1;
 	}
 	if((printf( "%s", buffer)==-1)){
 	  perror("read");
-	  
 	}
-	
-      }
+      }*/
       close(socket_client);
       exit(0);
-    }
+      }
+    
     close(socket_client);
-    
-  }
-
-    
+  } 
   return 0;
 }
 
