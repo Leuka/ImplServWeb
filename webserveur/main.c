176c5243beefe44f372e72fb2ba5f5344c9195b8
Remi OSWALT <remi.oswalt@etudiant.univ-lille1.fr>
2016-03-09 14:39:10 +0100

    premier GET qui va bien

--- webserveur/main.c
+++ webserveur/main.c
@@ -14,10 +14,13 @@ int main(void){
   int socket_client ;
   char buffer[1024] ="";
   const char *msg400 = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad Request\r\n";
-  const char *msg200 = "HTTP/1.1 200 OK\r\nContent-Length: length";
+  const char *msg200 = "HTTP/1.1 200 OK\r\nContent-Length: length\r\n";
   char *methode;
   char *ressources;
+  /*char *url;*/	
   char *spl;
+
+  const char * message_bienvenue = "*** Salut les  lapins ! *** \nBienvenue sur le serveur vachement interessant de la region Est qui remue les synapses et reveille les zygomatiques en hibernation. \nAnticyclone garanti.\n\n"  ;
 
   while( (socket_client= accepte_client(socket_serveur))!= -1 ){
     if(fork()==0){
@@ -25,38 +28,46 @@ int main(void){
       fgets(buffer, sizeof(buffer),fd);
 
       methode = strtok(buffer, " ");
-      strtok(NULL, " ");
+      /*url = */strtok(NULL, " ");
       ressources = strtok(NULL, " ");
-      
       spl = strtok(NULL, " ");
       
-      if ((strcmp(methode,"GET")==0) && (ressources != NULL) && (spl == NULL)){
-	if ((strcmp(ressources,"HTTP/1.0")==0) || (strcmp(ressources,"HTTP/1.1")==0)){
-	  fprintf(fd, "%s\n", msg200 );
-	  fflush(fd);
-	  fclose(fd);
-	}
-      } else {
-	fprintf(fd,"%s\n",msg400);
-	fflush(fd);
-	fclose(fd);
-      }
-      /*
+	printf("%s\n",methode );
+printf("%s\n",ressources );
+fprintf(fd, "%s\n", message_bienvenue );
+
+
+    if ((strcmp(methode,"GET")==0) && (ressources != NULL)&& (spl == NULL) ){
+		if (((strstr(ressources,"HTTP/1.0")==0) || (strstr(ressources,"HTTP/1.1")==0))){
+	//if (strcmp(url,"/")==0){
+		  fprintf(fd, "%s\n", message_bienvenue );
+		  fprintf(fd, "%s\n", msg200 );
+		  fflush(fd);
+		  fclose(fd);
+	//}
+		}
+		else {
+		  fprintf(fd,"%s\n",msg400);
+		  fflush(fd);
+		  fclose(fd);
+		}
+    }
+    /*
       while(strcmp("fin\n",buffer)!=0){	
-	if( fgets(buffer, sizeof(buffer), fd) == NULL){
-	  return -1;
-	}
-	if((printf( "%s", buffer)==-1)){
-	  perror("read");
-	}
-      }*/
-      close(socket_client);
-      exit(0);
+      if( fgets(buffer, sizeof(buffer), fd) == NULL){
+      return -1;
       }
-    
+      if((printf( "%s", buffer)==-1)){
+      perror("read");
+      }
+      }*/
     close(socket_client);
-  } 
-  return 0;
+    exit(0);
+  }
+    
+  close(socket_client);
+} 
+return 0;
 }
 
 
